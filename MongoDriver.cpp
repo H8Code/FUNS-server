d6bec6454b726f5583ef34d3d4e220d4c5c4aa6a
Viktor Filinkov <bruteforce@sigil.tk>
2016-02-24 19:17:07 +0600

    __find_id_and_exclude macro

--- MongoDriver.cpp
+++ MongoDriver.cpp
@@ -12,48 +12,53 @@ using bsoncxx::builder::stream::finalize;
 	for (auto &x: list) exclude_fields << x << 0; \
 	opts.projection(exclude_fields.view());
 
-mongocxx::options::find excluder(std::initializer_list<std::string> list)
-{
-	mongocxx::options::find opts;
-	bsoncxx::builder::stream::document exclude_fields;
-	for (auto &x : list) exclude_fields << x << 0;
-	opts.projection(exclude_fields.view());
-	return opts;
-}
+/*
+ * Will return all documents from collection, excluding fields "exclude"
+ */
+#define __find_all_and_exclude(collection, exclude) \
+	__excluder(exclude); \
+	auto cursor = db[collection].find({}, opts); \
+	std::string result{}; \
+	result += "["; \
+	for (auto &&doc : cursor) result += bsoncxx::to_json(doc) += ", "; \
+	result += "]"; \
+	return result;
+
+/*
+ * Will return documents from collection with matching id, excluding fields "exclude"
+ */
+#define __find_id_and_exclude(collection, id, exclude) \
+	__excluder(exclude); \
+	auto cursor = db[collection] \
+	.find( document{} << "_id" << id << finalize, opts); \
+	return bsoncxx::to_json(*cursor.begin());
+
+/*
+ * List type for macro-lists
+ */
+using __list_t = std::initializer_list<std::string>;
 
 std::string MongoDriver::get_schedules()
 {
-	__excluder(std::initializer_list<std::string>({"odd", "even", "unusual", "subjects"}));
-	auto cursor = db[mongo_config::c_schedules]
-	.find({}, opts);
-	
-	std::string result{};
-	result += "[";
-	for (auto &&doc : cursor)
-		result += bsoncxx::to_json(doc) += ", ";
-	result += "]";
-	return result;
+	__list_t exclude = {"odd", "even", "unusual", "subjects"};
+	__find_all_and_exclude(mongo_config::c_schedules, exclude)
 }
 
 std::string MongoDriver::get_schedule_by_id(std::string id)
 {
-	__excluder(std::initializer_list<std::string>({"odd", "even", "unusual", "subjects"}));
-	auto cursor = db[mongo_config::c_schedules]
-		.find(
-		document{}
-	<< "_id" << bsoncxx::oid(id) << finalize,
-		opts);
-	return bsoncxx::to_json(*cursor.begin());
+	__list_t exclude = {"odd", "even", "unusual", "subjects"};
+	__find_id_and_exclude(mongo_config::c_schedules, bsoncxx::oid(id), exclude)
 }
 
 std::string MongoDriver::get_users()
 {
-	return " ";
-}
+	__list_t exclude = {"password_hash", "salt"};
+	__find_all_and_exclude(mongo_config::c_users, exclude)}
 
 std::string MongoDriver::get_users_by_id(std::string id)
 {
-	return " ";
+	__list_t exclude = {"password_hash", "salt"};
+	__find_id_and_exclude(mongo_config::c_users, id, exclude)
 }
 
 void MongoDriver::checks()
