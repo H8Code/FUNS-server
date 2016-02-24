8d10a8248b2d7eb523d2dfa0ce3edddab783c460
Viktor Filinkov <bruteforce@sigil.tk>
2016-02-24 19:16:47 +0600

    Formatting

--- RESTService.cpp
+++ RESTService.cpp
@@ -5,24 +5,24 @@
 #include "api/Users.h"
 #include "api/UsersID.h"
 
-RESTService::RESTService(shared_ptr<DBDriver> _db):
-    db{_db},
-    settings{make_shared<Settings>()},
-    service{make_shared<Service>()}
+RESTService::RESTService(shared_ptr<DBDriver> _db) :
+db{_db},
+settings{make_shared<Settings>()},
+service{make_shared<Service>()}
 {
-    settings->set_port(funs_config::port);
-    settings->set_bind_address(funs_config::bind_addres);
-    settings->set_default_header( "Connection", "close" );
-    settings->set_worker_limit(funs_config::worker_limit);
+	settings->set_port(funs_config::port);
+	settings->set_bind_address(funs_config::bind_addres);
+	settings->set_default_header("Connection", "close");
+	settings->set_worker_limit(funs_config::worker_limit);
 
-    service->publish(make_shared<APISchedules>(db, resources::schedules));
-    service->publish(make_shared<APISchedulesID>(db, resources::schedules_id));
-    service->publish(make_shared<APIUsers>(db, resources::users));
-    service->publish(make_shared<APIUsersID>(db, resources::users_id));
+	service->publish(make_shared<APISchedules>(db, resources::schedules));
+	service->publish(make_shared<APISchedulesID>(db, resources::schedules_id));
+	service->publish(make_shared<APIUsers>(db, resources::users));
+	service->publish(make_shared<APIUsersID>(db, resources::users_id));
 }
 
 void RESTService::start()
 {
-    service->start(settings);
+	service->start(settings);
 }
 
--- RESTService.h
+++ RESTService.h
@@ -10,25 +10,23 @@ using namespace restbed;
 using namespace std;
 
 namespace resources {
-    constexpr auto schedules = "/api/schedules";
-    constexpr auto schedules_id = "/api/schedules/{id: .*}";
-    constexpr auto users = "/api/users";
-    constexpr auto users_id = "/api/users/{id: .*}";
+	constexpr auto schedules = "/api/schedules";
+	constexpr auto schedules_id = "/api/schedules/{id: .*}";
+	constexpr auto users = "/api/users";
+	constexpr auto users_id = "/api/users/{id: .*}";
 }
 
-class RESTService
-{
-    
+class RESTService {
 public:
-    RESTService() = delete;
-    RESTService(shared_ptr<DBDriver> db);
+	RESTService() = delete;
+	RESTService(shared_ptr<DBDriver> db);
 
-    void start();
+	void start();
 
 private:
-    shared_ptr<DBDriver> db;
-    shared_ptr<Settings> settings;
-    shared_ptr<Service> service;
+	shared_ptr<DBDriver> db;
+	shared_ptr<Settings> settings;
+	shared_ptr<Service> service;
 };
 
 #endif // SERVICE_H
