85279aed554f7cc3a229bef95032307fe9471e80
Viktor Filinkov <bruteforce@sigil.tk>
2016-02-24 19:30:28 +0600

    remove const qualifiers from mongo_config, that are not necessary

--- MongoConfig.h
+++ MongoConfig.h
@@ -2,10 +2,10 @@
 #define MONGODBCOLLECTIONS
 
 namespace mongo_config {
-constexpr auto const db_name{"funs"};
-constexpr auto const c_test{"test_collection"};
-constexpr auto const c_schedules{"schedules"};
-constexpr auto const c_users{"users"};
+constexpr auto db_name{"funs"};
+constexpr auto c_test{"test_collection"};
+constexpr auto c_schedules{"schedules"};
+constexpr auto c_users{"users"};
 }
 
 #endif // MONGODBCOLLECTIONS
