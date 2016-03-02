26fd27c3cd433a788bede5b5f2d86e45cabed43b
Viktor Filinkov <bruteforce@sigil.tk>
2016-03-02 22:24:47 +0600

    token_header = Authorzation

--- FunsConfig.h
+++ FunsConfig.h
@@ -6,7 +6,7 @@ namespace config {
 	constexpr auto port = 34666;
 	constexpr auto bind_addres = "127.0.0.1";
 	constexpr auto worker_limit = 4;
-	constexpr auto token_header = "Token";
+	constexpr auto token_header = "Authorization";
 	constexpr auto token_lenght = 32;
 	constexpr auto salt_lenght = 32;
 }
